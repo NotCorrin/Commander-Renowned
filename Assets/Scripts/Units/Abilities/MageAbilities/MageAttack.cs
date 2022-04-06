0efee0a2ee31304fb78102fe839c17fcc7cf07c8
Tristan13917733 <13917733@student.uts.edu.au>
2022-04-07 00:17:45 +1000

    Finished Mage
    Completed mage abilities
    completed mage prefab

--- Assets/Scripts/Units/Abilities/MageAbilities/MageAttack.cs
+++ Assets/Scripts/Units/Abilities/MageAbilities/MageAttack.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MageAttack : Ability
-{ 
+{
+	[SerializeField] int Damage;
+	[SerializeField] int Cost;
 	public override bool IsAbilityValid (Unit Caster, Unit Target) {
 		//does the unit have mana
 		if(Caster is MagicUnit) {
 			MagicUnit magicUnit = Caster as MagicUnit;
-			return magicUnit.Mana > 0;
+			return magicUnit.Mana > Cost;
 		} else if (Caster is CommanderUnit) {
 			CommanderUnit casterUnit = Caster as CommanderUnit;
-			return casterUnit.Mana > 0;
+			return casterUnit.Mana > Cost;
 		}
 		return false;
 	}
 	public override void UseAbility (Unit Caster, Unit Target) {
 		if (IsAbilityValid(Caster, Target)) {
-			GameEvents.onHealthChanged(Target, -1);
-			GameEvents.onUseMana(Caster, 1);
+			GameEvents.onHealthChanged(Target, GetDamageCalculation(Caster, Target, Damage));
+			GameEvents.onUseMana(Caster, Cost);
 		}
 	}
 	public override int GetMoveWeight () {
