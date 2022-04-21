62c206bd51baf9ca0ac32a9c61fa84eac1d06fc6
Mateen Mahmood <43767802+NotCorrin@users.noreply.github.com>
2022-04-21 13:04:21 +1000

    Made billboard more streamlined

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -16,17 +16,42 @@ public class Billboard : MonoBehaviour
         camTransform = Camera.main.transform;
     }
 
+    void Update()
+    {
+        // if spacebar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchBillboardState();
+        }
+    }
+
     void LateUpdate()
     {
         if (!useStaticBillboard)
         {
-            transform.LookAt(camTransform);
+            Vector3 direction = camTransform.position - transform.position;
+            // transform.LookAt(camTransform);
+            // transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y - 180f, 0f);
+            direction.x = 0;
+            direction.z = 0;
+            Quaternion toRotation = Quaternion.FromToRotation(transform.forward, direction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, Time.deltaTime * 5f);
         }
         else
         {
-            transform.rotation = camTransform.rotation;
-        }
+            Quaternion endRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, 0f);
 
-        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+            transform.rotation = camTransform.rotation;
+            // transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0f, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, endRotation, Time.deltaTime);
+        }
+    }
+
+    void SwitchBillboardState()
+    {
+        useStaticBillboard = !useStaticBillboard;
+
+        // Lerp 
+        transform.rotation = Quaternion.Lerp(transform.rotation, camTransform.rotation, Time.deltaTime);
     }
 }
