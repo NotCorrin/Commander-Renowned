bb933f1442a20058b3b93eb6df1fb3f4185690f6
Mateen Mahmood <43767802+NotCorrin@users.noreply.github.com>
2022-04-23 08:19:36 +1000

    Added UI Elements for Health, Mana and Ammo with their Respective Scripts

--- Assets/Scripts/UI/HealthbarUI.cs
+++ Assets/Scripts/UI/HealthbarUI.cs
@@ -5,30 +5,35 @@ using UnityEngine.UIElements;
 
 public class HealthbarUI : MonoBehaviour
 {
-    [SerializeField] private Transform transformToFollow;
+    [SerializeField] private UIDocument uiDocument;
     private VisualElement healthbarContainer;
     private TextElement healthbarValue;
-
     private Camera cam;
 
     void Start()
     {
-        healthbarContainer = GetComponent<UIDocument>().rootVisualElement.Query<VisualElement>("Container");
         cam = Camera.main;
 
-        if (healthbarContainer != null)
+        if (uiDocument == null)
         {
-            // healthbarValue = healthbarContainer.Query<TextElement>("HealthValue");
+            Debug.Log($"{gameObject.name} : HealthbarUI - has no UIDocument assigned in the inspector. Script will still work, but is not 100% safe.");
+            uiDocument = GetComponentInParent<UIDocument>();
         }
-        else
+
+        try
         {
-            Debug.LogError("HealthbarUI: Could not find container");
+            healthbarContainer = uiDocument.rootVisualElement.Query<VisualElement>("container");
+            healthbarValue = healthbarContainer.Query<TextElement>("value");
+        }
+        catch
+        {
+            Debug.LogError($"{gameObject.name} : HealthbarUI - Element Query Failed.");
         }
     }
 
     void LateUpdate()
     {
-        Vector2 newPosition = RuntimePanelUtils.CameraTransformWorldToPanel(healthbarContainer.panel, transformToFollow.position, cam);
+        Vector2 newPosition = RuntimePanelUtils.CameraTransformWorldToPanel(healthbarContainer.panel, transform.position, cam);
         healthbarContainer.transform.position = new Vector3 (newPosition.x - healthbarContainer.layout.width / 2, newPosition.y - healthbarContainer.layout.height / 2, 0);
     }
 }
