f6723a6a0bae8dc752e727546c22f94f38a98965
Mateen Mahmood <43767802+NotCorrin@users.noreply.github.com>
2022-05-03 13:41:31 +1000

    Added Camera Movement Between Phases

---
+++ Assets/Scripts/Cinemachine/CameraController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : Listener
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private float moveSpeed = 0.5f;
+    private CinemachineTrackedDolly dolly;
+
+    protected override void SubscribeListeners()
+    {
+        GameEvents.onPhaseChanged += CameraHandler;
+    }
+
+    protected override void UnsubscribeListeners()
+    {
+        GameEvents.onPhaseChanged -= CameraHandler;
+    }
+
+    void Start()
+    {
+        dolly = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+    }
+
+    void CameraHandler(RoundController.Phase phase)
+    {
+        if ((phase == RoundController.Phase.PlayerSupport ||
+             phase == RoundController.Phase.PlayerSwap ||
+             phase == RoundController.Phase.EnemySupport ||
+             phase == RoundController.Phase.EnemySwap) &&
+             dolly.m_PathPosition != 1)
+        {
+            StartCoroutine(MoveCameraUp());
+        }
+
+        else if ((phase == RoundController.Phase.EnemyVangaurd ||
+                 phase == RoundController.Phase.PlayerVanguard) &&
+                 dolly.m_PathPosition != 0)
+        {
+            StartCoroutine(MoveCameraDown());
+        }
+    }
+
+    IEnumerator MoveCameraUp()
+    {
+        float startTime = Time.time;
+
+        while (Time.time < startTime + moveSpeed)
+        {
+            dolly.m_PathPosition = (Time.time - startTime) / moveSpeed;
+            yield return null;
+        }
+        dolly.m_PathPosition = 1f;
+    }
+
+    IEnumerator MoveCameraDown()
+    {
+        float startTime = Time.time;
+
+        while (Time.time < startTime + moveSpeed)
+        {
+            dolly.m_PathPosition = 1f - (Time.time - startTime) / moveSpeed;
+            yield return null;
+        }
+        dolly.m_PathPosition = 0f;
+    }
+}
