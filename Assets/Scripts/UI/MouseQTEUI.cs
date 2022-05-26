166d50a5e6decacf4a3185f5cc04a2fb9c651407
NotCorrin <43767802+NotCorrin@users.noreply.github.com>
2022-05-26 17:15:39 +1000

    Added QTE UI Document

--- Assets/Scripts/UI/MouseQTEUI.cs
+++ Assets/Scripts/UI/MouseQTEUI.cs
@@ -5,24 +5,28 @@ using UnityEngine.UIElements;
 
 public class MouseQTEUI : Listener
 {
+    [Header("Debugging")]
+    [Range(0, 100), SerializeField] private int critChance = 10;
+    [Range(0, 100), SerializeField] private int normalChance = 50;
+    
+    [Header("UI Elements")]
     [SerializeField] private UIDocument uiDocument;
-    private VisualElement container;
-    private VisualElement qteCircle;
-    private bool IsClicked = false;
+    private VisualElement container, arrow, normal, crit;
+    private TextElement statusLabel;
+    private bool isMovingLeft;
+    private int currentLocation;
 
     protected override void SubscribeListeners()
     {
-        GameEvents.onQTEStart += QTEAnimation;
         GameEvents.onQTEResolved += EndQTE;
     }
 
     protected override void UnsubscribeListeners()
     {
-        GameEvents.onQTEStart -= QTEAnimation;
         GameEvents.onQTEResolved -= EndQTE;
     }
 
-    void Start()
+    void Awake()
     {
         if (uiDocument == null)
         {
@@ -32,58 +36,119 @@ public class MouseQTEUI : Listener
 
         try
         {
-            container = uiDocument.rootVisualElement.Q<VisualElement>("container");
-            qteCircle = container.Query<VisualElement>("qte-circle");
+            container = uiDocument.rootVisualElement.Query<VisualElement>("container");
+
+            arrow = container.Query<VisualElement>("arrow");
+            normal = container.Query<VisualElement>("normal");
+            crit = container.Query<VisualElement>("crit");
+
+            statusLabel = container.Query<TextElement>("status");
         }
         catch
         {
-            Debug.LogError($"{gameObject.name} : AttackBuffUI - Element Query Failed.");
+            Debug.LogError($"{gameObject.name} : Mouse QTE UI - Element Query Failed.");
         }
 
-        qteCircle.style.scale = new Vector2(1, 1);
+        normal.style.width = new Length(normalChance, LengthUnit.Percent);
+        crit.style.width = new Length(critChance, LengthUnit.Percent);
 
-        container.style.opacity = 1;
+        container.RegisterCallback<ClickEvent>(OnClick);
+        statusLabel.RegisterCallback<TransitionEndEvent>(OnStatusTransitionEnd);
+        arrow.RegisterCallback<TransitionEndEvent>(OnTransitionEnd);
+        StartCoroutine(LateStart());
     }
 
-    private void Update()
+    IEnumerator LateStart()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            MenuEvents.QTETriggered();
-
-            IsClicked = true;
-        }
+        yield return new WaitForSeconds(0.6f);
+        arrow.style.left = new Length(5, LengthUnit.Percent);
+        isMovingLeft = false;
     }
 
-    private void QTEAnimation(QTEController.QTEType qteType, int difficultyModifier)
+    void OnClick(ClickEvent evt)
     {
-        qteCircle.style.transitionDuration = new StyleList<TimeValue>(new List<TimeValue> { new TimeValue(QTEController.ShrinkingCircleMaxTime, TimeUnit.Second) });
+        container.UnregisterCallback<ClickEvent>(OnClick);
+        arrow.UnregisterCallback<TransitionEndEvent>(OnTransitionEnd);
+        Debug.Log($"Clicked {currentLocation}");
 
-        //Debug.Log(qteCircle.style.scale);
-
-        if (!IsClicked)
+        if (currentLocation >= 50 - (critChance / 2) && currentLocation <= 50 + (critChance / 2))
         {
-            qteCircle.style.scale = new Scale(new Vector2(0f, 0));
-
-            if (qteCircle.style.scale == new Scale(new Vector2(0f, 0f)))
-            {
-                container.style.transitionDelay = new StyleList<TimeValue>(new List<TimeValue> { new TimeValue(QTEController.ShrinkingCircleMaxTime, TimeUnit.Second) });
-
-                container.style.opacity = 0;
-            }
+            Debug.Log("Crit");
+            statusLabel.text = "CRIT!";
+            statusLabel.style.color = new StyleColor(new Color((0f / 255f), (152f / 255f), (220f / 255f)));
         }
-        else if (IsClicked)
+        else if (currentLocation >= 50 - (normalChance / 2) && currentLocation <= 50 + (normalChance / 2))
         {
-            qteCircle.style.scale = new Scale(new Vector2(Time.deltaTime, Time.deltaTime));
-
-            container.style.opacity = 0;
+            Debug.Log("Normal");
+            statusLabel.text = "Hit!";
+            statusLabel.style.color = new StyleColor(new Color(1f, 1f, 1f));
         }
+        else
+        {
+            Debug.Log("Miss");
+            statusLabel.text = "Miss...";
+            statusLabel.style.color = new StyleColor(new Color(1f, 1f, 1f, 0.69f));
+        }
+
+        statusLabel.style.scale = new Scale(new Vector2(3, 3));
     }
 
-    void LateUpdate()
+    void OnTransitionEnd(TransitionEndEvent evt)
     {
-        QTEAnimation(QTEController.QTEType.shrinkingCircle, 1);
+        currentLocation += isMovingLeft ? -1 : 1;
+        isMovingLeft = currentLocation == 0 || currentLocation == 100 ? !isMovingLeft : isMovingLeft;
+
+        arrow.style.left = new Length(currentLocation, LengthUnit.Percent);
     }
+
+    void OnStatusTransitionEnd(TransitionEndEvent evt)
+    {
+        statusLabel.UnregisterCallback<TransitionEndEvent>(OnStatusTransitionEnd);
+        // Set transition duration
+        statusLabel.style.transitionDuration = new List<TimeValue> { new TimeValue(1000f, TimeUnit.Millisecond) };
+        statusLabel.style.opacity = 0f;
+        statusLabel.style.scale = new Scale(new Vector2(1, 1));
+    }
+
+    // private void Update()
+    // {
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         MenuEvents.QTETriggered();
+
+    //         IsClicked = true;
+    //     }
+    // }
+
+    // private void QTEAnimation(QTEController.QTEType qteType, int difficultyModifier)
+    // {
+    //     qteCircle.style.transitionDuration = new StyleList<TimeValue>(new List<TimeValue> { new TimeValue(QTEController.ShrinkingCircleMaxTime, TimeUnit.Second) });
+
+    //     //Debug.Log(qteCircle.style.scale);
+
+    //     if (!IsClicked)
+    //     {
+    //         qteCircle.style.scale = new Scale(new Vector2(0f, 0));
+
+    //         if (qteCircle.style.scale == new Scale(new Vector2(0f, 0f)))
+    //         {
+    //             container.style.transitionDelay = new StyleList<TimeValue>(new List<TimeValue> { new TimeValue(QTEController.ShrinkingCircleMaxTime, TimeUnit.Second) });
+
+    //             container.style.opacity = 0;
+    //         }
+    //     }
+    //     else if (IsClicked)
+    //     {
+    //         qteCircle.style.scale = new Scale(new Vector2(Time.deltaTime, Time.deltaTime));
+
+    //         container.style.opacity = 0;
+    //     }
+    // }
+
+    // void LateUpdate()
+    // {
+    //     QTEAnimation(QTEController.QTEType.shrinkingCircle, 1);
+    // }
 
     void EndQTE(QTEController.QTEResult result)
     {
